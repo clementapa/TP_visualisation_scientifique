fccced48f91acffe0504dc762608c63b68ef1e71
Apavou Clément <clementapa93@hotmail.fr>
2021-01-08 17:31:15 +0100

    reglages problème

--- TP1/build/CMakeFiles/TP1.dir/DependInfo.cmake
+++ TP1/build/CMakeFiles/TP1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/clement/Cours/5A/Visualisation_scientifique/TP1/main.cpp" "/home/clement/Cours/5A/Visualisation_scientifique/TP1/build/CMakeFiles/TP1.dir/main.cpp.o"
+  "/home/clement/Cours/5A/Visualisation_scientifique/TP_visualisation_scientifique/TP1/main.cpp" "/home/clement/Cours/5A/Visualisation_scientifique/TP_visualisation_scientifique/TP1/build/CMakeFiles/TP1.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- TP1/build/cmake_install.cmake
+++ TP1/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/clement/Cours/5A/Visualisation_scientifique/TP1
+# Install script for directory: /home/clement/Cours/5A/Visualisation_scientifique/TP_visualisation_scientifique/TP1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/bin")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -44,7 +44,7 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
          FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/TP1"
          RPATH "")
   endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/clement/Cours/5A/Visualisation_scientifique/TP1/build/TP1")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/clement/Cours/5A/Visualisation_scientifique/TP_visualisation_scientifique/TP1/build/TP1")
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/TP1" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/TP1")
     file(RPATH_CHANGE
@@ -65,5 +65,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/clement/Cours/5A/Visualisation_scientifique/TP1/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/clement/Cours/5A/Visualisation_scientifique/TP_visualisation_scientifique/TP1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
